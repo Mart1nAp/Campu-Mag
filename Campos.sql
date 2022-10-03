3c862ed576396969a11d5516f9efa3885d31d84c
Mart1nAp <111818427+Mart1nAp@users.noreply.github.com>
2022-10-03 14:59:29 -0500

    Add files via upload

---
+++ Campos.sql
@@ -0,0 +1,10 @@
+INSERT INTO campo (nombre, ubicacion, hoyos, yardaje, clima) VALUES
+
+("San Andres golf club", "Kilometro dos - vía a La Punta -Funza", 18, 5740, "frio"),
+("Carmel golf club", "Autopista Norte #153-81", 18, 6408, "frio"),
+("Pueblo viejo country club", "Carretera Suba-Cota, kilómetro 7", 18, 7450, "frio"),
+("UMB golf club", "Tocancipa Kilometro 2 Vía Zipaquirá Vereda La Fuente", 18, 6831, "frio")
+
+
+
+usuarios
